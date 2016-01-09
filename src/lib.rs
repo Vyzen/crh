bf73b13b7517e9a0caf3dccdd2b92f7b26669a5f
Michael Thorsley <michael.thorsley+github@gmail.com>
2016-01-09 17:35:06 -0500

    Baseline commit.  Partial implementation of binary heap.

---
+++ src/lib.rs
@@ -0,0 +1,151 @@
+use std::cmp::Eq;
+use std::cmp::Ord;
+
+/// Specification for a data type that implements a
+/// min-heap.
+pub trait Heap<T : Eq + Ord> {
+
+	/// Add an element to this heap.
+	fn add(&mut self, x:T);
+	
+	/// Get the minimum element of this heap.
+	fn peek_min(&self) -> Option<&T>;
+	
+	/// Removes the minimum element of this heap.
+	fn remove_min(&mut self) -> Option<T>;
+	
+	/// Gets the current size of this heap.
+	fn size(&self) -> usize;
+}
+
+/// A basic Binomial heap implementation based on a vector.
+pub struct BinomialHeap<T : Eq + Ord> {
+	data: Vec<T>
+}
+
+impl<T : Eq + Ord> BinomialHeap<T> {
+	fn new() -> BinomialHeap<T> {
+		BinomialHeap::<T> {
+			data : Vec::new()
+		}
+	}
+		
+	fn left(&self, idx: usize) -> Option<usize> {
+		let proposed = 2*idx + 1;
+		if proposed < self.data.len() { Some(proposed) }
+		else { None }
+	}
+	
+	fn right(&self, idx: usize) -> Option<usize> {
+		let proposed = 2*idx + 2;
+		if proposed < self.data.len() { Some(proposed) }
+		else { None }
+	}
+	
+	fn parent(&self, idx: usize) -> Option<usize> {
+		if idx == 0 { None }
+		else { Some((idx - 1) / 2) }
+	}
+	
+	fn trickle_up(&mut self, idx: usize) {
+		let parent = self.parent(idx);
+		match parent {
+			Some(parent_idx) => {
+				if self.data[parent_idx] > self.data[idx] {
+					self.data.swap(idx, parent_idx);
+					self.trickle_up(parent_idx);
+				}
+			}
+			None => {}
+		}
+	}
+	
+	fn heapify(&mut self, idx: usize) {
+		let left = self.left(idx);
+		let right = self.right(idx);
+		match left {
+			Some(left_idx) => {
+				match right { 
+					Some(right_idx) => {
+						// Both children exist.
+						if self.data[idx] > self.data[left_idx] || self.data[idx] > self.data[right_idx] {
+							// promote the smaller one.
+							let smaller_idx = if self.data[left_idx] < self.data[right_idx] { left_idx } else { right_idx };
+							self.data.swap(idx, smaller_idx);
+							self.heapify(smaller_idx);
+						} 
+					}
+					None => {
+						// Only left exists.
+						if self.data[idx] > self.data[left_idx] {
+							self.data.swap(idx, left_idx);
+							self.heapify(left_idx);
+						}
+					}
+				}
+			}
+			
+			None => {
+				// If left does not exist, right can't exist either.
+			}
+		}
+	}
+}
+
+impl<T : Eq + Ord> Heap<T> for BinomialHeap<T> {
+	fn add(&mut self, x:T) { 
+		let idx = self.data.len();
+		self.data.push(x); 
+		self.trickle_up(idx);
+	}
+
+	fn size(&self) -> usize { self.data.len() }
+	
+	fn peek_min(&self) -> Option<&T> {
+		if self.data.is_empty() { None }
+		else { None }
+	}
+	
+	fn remove_min(&mut self) -> Option<T> {
+		if self.data.is_empty() { None }
+		else {
+			let ret = Some(self.data.swap_remove(0));
+			self.heapify(0);
+			ret
+		}
+	}
+
+}
+
+#[test]
+fn binomial_heap_creation() {
+	let mut h = BinomialHeap::<i32>::new();
+	h.add(3);
+	h.add(4);
+	h.add(2);
+	assert!(h.size() == 3);
+}
+
+#[test]
+fn binomial_heap_remove_min() {
+	let mut h = BinomialHeap::<i32>::new();
+	h.add(3);
+	h.add(4);
+	h.add(2);
+	assert!(h.size() == 3);
+	
+	let x = h.remove_min().unwrap();
+	assert!(x == 2);
+	assert!(h.size() == 2);
+	
+	let y = h.remove_min().unwrap();
+	assert!(y == 3);
+	assert!(h.size() == 1);
+	
+	let z = h.remove_min().unwrap();
+	assert!(z == 4);
+	assert!(h.size() == 0);
+	
+	assert!(h.remove_min().is_none());
+	
+}
