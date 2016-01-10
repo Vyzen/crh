fe69bff1bb6e6f279166e885bec5f5f1c2460119
Michael Thorsley <michael.thorsley+github@gmail.com>
2016-01-09 19:02:05 -0500

    Separated base trait and specfic implementation into different files.

---
+++ src/heap.rs
@@ -0,0 +1,20 @@
+
+use std::cmp::Eq;
+use std::cmp::Ord;
+
+/// Specification for a data type that implements a
+/// min-heap.
+pub trait Heap<T : Eq + Ord> {
+
+	/// Add an element to this heap.
+	fn add(&mut self, x:T);
+
+	/// Get the minimum element of this heap.
+	fn peek_min(&self) -> Option<&T>;
+
+	/// Removes the minimum element of this heap.
+	fn remove_min(&mut self) -> Option<T>;
+
+	/// Gets the current size of this heap.
+	fn size(&self) -> usize;
+}
